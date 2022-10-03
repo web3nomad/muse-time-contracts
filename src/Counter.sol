a2683b3becaa90171fd0a9a5c02d83c3415bc3fc
web3nomad.eth <me@web3nomad.eth>
2022-10-03 15:11:27 +0800

    update remappings

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Counter {
+import "solmate/auth/Owned.sol";
+import "solmate/tokens/ERC721.sol";
+
+contract Counter is Owned {
     uint256 public number;
+
+    constructor() Owned(msg.sender) {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
